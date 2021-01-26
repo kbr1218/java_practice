7476be76f9fd2dc2350c04756515065d9390c692
KBR <KBR@LAPTOP-L87QGVGK>
2021-01-27 00:42:56 +0900

    연습확인용

--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -2,8 +2,8 @@
 public class GugudanMain {
     public static void main(String[] args) {
     	for(int i = 2; i < 10; i++) {
-        	int[] result = Gugudan.calculate(i);
-        	Gugudan.print(result);
+        	int[] result = Gugudan.calculate(i);		//class 이름은 대문자로 시작, method 이름은 소문자로 시작
+        	Gugudan.print(result);						//class 이름을 불러와야 method를 쓸 수 있음
     	}
     }
 }
